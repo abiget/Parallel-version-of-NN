d272bff3885b1b7f89f46d02b83f51fe7b6f4d55
Abi <getachewante@gmail.com>
2021-07-16 11:35:33 +0300

    time change

--- serial/main.c
+++ serial/main.c
@@ -1,12 +1,29 @@
 #include "nn.h"
-#include <omp.h>
-#include<time.h>
+// #include <omp.h>
+// #include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+double getTime()
+{
+  const double kMicro = 1.0e-6;
+  struct timeval TV;
+
+  const int RC = gettimeofday(&TV, NULL);
+  if(RC == -1)
+    {
+      printf("ERROR: Bad call to gettimeofday\n");
+      return(-1);
+    }
+  return( ((double)TV.tv_sec) + kMicro * ((double)TV.tv_usec) );
+}
+
 int main()
 {
 	// srand( static_cast<unsigned int>(time(NULL)));
-    srand( (unsigned int) time(NULL) );
+    // srand( (unsigned int) time(NULL) );
 	float start,stop;
-	start = omp_get_wtime();
+	start = getTime();
     Network network;
     initNetwork(&network);
 
@@ -17,9 +34,9 @@ int main()
         trainNetwork(&network);
         testNetwork(&network);
     }
-    stop = omp_get_wtime();
-    float elapse=stop-start;
+    stop = getTime();
+    float serialTime= stop-start;
     printf("Serial Implementation of NN\n");
-    printf("\nTime Elapsed=%f",elapse );
+    printf("\nTime Elapsed=%f",serialTime );
     return 0;
 }

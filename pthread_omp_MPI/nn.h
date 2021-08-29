3a39cd49726d5f31f9184c218c7e12a2613852db
Lucky isaac <isaaclucky88@gmail.com>
2021-08-29 22:32:09 +0300

    aug 29 random

---
+++ pthread_omp_MPI/nn.h
@@ -0,0 +1,36 @@
+#include "dataLoader.h"
+#include "config.h"
+// #if !defined()
+// #include "../mnist.h"
+// #endif
+typedef struct Node_{
+    double bias;
+    double output;
+    double backPropValue;
+    int numberOfWeights;
+    double* weights;
+} Node;
+
+typedef struct Layer_{
+    int numberOfNodes;
+    Node* nodes;
+} Layer;
+
+typedef struct Network_{
+    Layer inputLayer;
+    Layer hiddenLayer;
+    Layer outputLayer;
+} Network;
+typedef struct Networks_{
+    int numberOfNetworks;
+    Network * network;
+} Networks;
+typedef struct threadInfo_{
+    Network * network;
+    int start, end;
+}ThreadInfo;
+
+void initNetworks(Networks* networks);
+void initNetwork(Network * network);
+void * trainNetwork(void *myThreadInfo);
+void testNetwork(Network *network);
